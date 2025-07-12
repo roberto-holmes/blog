f6199914541bb05cb775e53c56ad9fdbdbf69cf3
Roberto Holmes <robertoholmes@live.com>
2025-07-12 12:05:30 +0100

    Set up for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://robertoholm.es",
+	base: "blog",
 	integrations: [mdx(), sitemap()],
 });
