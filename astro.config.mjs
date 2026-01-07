89df6d9c9002f26eae7ce85aff1f664568a97c9f
Alex Adler <project.alex.adler@gmail.com>
2026-01-07 17:38:56 +0100

    Added automated hyperlinks to every header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,29 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://robertoholm.es",
 	base: "blog",
 	integrations: [mdx(), sitemap()],
-	// i18n: {
-	// 	locales: ["es", "en"],
-	// 	defaultLocale: "en",
-	// },
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "wrap",
+					headingProperties: {
+						className: ["anchor"],
+					},
+					properties: {
+						className: ["anchor-link"],
+					},
+				},
+			],
+		],
+	},
 });
