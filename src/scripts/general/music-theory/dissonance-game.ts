f17e799253c95b6b949a3d4c84b8c3aed014e793
Roberto Holmes <robertoholmes@live.com>
2026-08-13 08:59:02 +0100

    Implemented quick sort for the inputs to the dissonance game

--- src/scripts/general/music-theory/dissonance-game.ts
+++ src/scripts/general/music-theory/dissonance-game.ts
@@ -37,12 +37,59 @@ class Tone {
     }
 }
 
+class Bin {
+    sameCount: number;
+    values: number[];
+    constructor(array: number[]) {
+        this.sameCount = 0;
+        this.values = array;
+    }
+    push(value: number) {
+        this.values.push(value);
+    }
+    pushArray(array: number[]) {
+        this.values.push.apply(this.values, array);
+    }
+    addSame(value: number) {
+        this.values.push(value);
+        this.sameCount++;
+    }
+    getPivot(): number {
+        if (this.values[0] === undefined) {
+            console.error("tried to get the pivot of an empty bin");
+            return 0;
+        }
+        return this.values[0];
+    }
+    popNextValue(): number {
+        // let v = this.values[this.sameCount + 1] as number;
+        // this.values.splice(this.sameCount + 1, 1);
+        let v = this.values[1] as number;
+        this.values.splice(1, 1);
+        return v;
+    }
+    getNextValue(): number {
+        return this.values[1] as number;
+    }
+    isComplete(): boolean {
+        return this.values.length === this.sameCount + 1;
+    }
+}
+
 class Game {
     context: AudioContext;
     counter: HTMLElement;
 
     currentRound: number;
-    rounds: number[][];
+    maxRounds: number;
+
+    // Variables for keeping track of the sorting
+    sortingArray: Bin[];
+    sortingCurrentBin: number;
+    // Keep track of if the current bin has created bins for the values that are smaller and bigger than the pivot
+    sortingLessBin: boolean;
+    sortingMoreBin: boolean;
+    sortingComplete: boolean;
 
     buttons: HTMLElement[];
     volumeSlider: HTMLInputElement;
@@ -57,38 +104,38 @@ class Game {
         this.tone1 = new Tone(context);
         this.tone2 = new Tone(context);
 
-        let intervals: number[] = [];
+        let intervals = [];
         for (let i = 0; i <= 1.0; i += 1.0 / intervalCount) {
             intervals.push(i);
         }
+        // We actually have one more interval because we need to count the interval 0
+        intervalCount++;
 
         this.currentRound = 0;
-        this.rounds = [];
-        for (let i = 0; i < intervals.length - 1; i++) {
-            for (let j = i + 1; j < intervals.length; j++) {
-                // Randomise the order that the notes are played in
-                if (Math.random() < 0.5) {
-                    this.rounds.push([intervals[i] as number, intervals[j] as number]);
-                } else {
-                    this.rounds.push([intervals[j] as number, intervals[i] as number]);
-                }
-            }
-        }
+        this.maxRounds = (intervalCount * (intervalCount - 1)) / 2;
 
-        let currentIndex = this.rounds.length;
+        console.log(`Max tests: ${this.maxRounds}`);
 
         // Fisher–Yates shuffle
+        let currentIndex = intervals.length;
         while (currentIndex != 0) {
             // Pick a remaining element...
             let randomIndex = Math.floor(Math.random() * currentIndex--);
 
-            if (this.rounds[currentIndex] === undefined || this.rounds[randomIndex] === undefined) {
-                console.warn("Issue with shuffling the rounds");
+            if (intervals[currentIndex] === undefined || intervals[randomIndex] === undefined) {
+                console.warn("Issue with shuffling intervals");
                 break;
             }
             // And swap it with the current element.
-            [this.rounds[currentIndex], this.rounds[randomIndex]] = [this.rounds[randomIndex], this.rounds[currentIndex] as number[]];
+            [intervals[currentIndex], intervals[randomIndex]] = [intervals[randomIndex], intervals[currentIndex] as number];
         }
+
+        // Prepare the data structure for sorting the intervals
+        this.sortingArray = [new Bin(intervals)];
+        this.sortingCurrentBin = 0;
+        this.sortingLessBin = false;
+        this.sortingMoreBin = false;
+        this.sortingComplete = false;
 
         this.buttons = [
             document.getElementById("selector-A") as HTMLElement,
@@ -106,31 +153,73 @@ class Game {
             this.select("3");
         });
 
-        console.log(intervals);
-        console.log(this.rounds);
-
-        this.updateCounter();
+        // console.log(intervals);
+        // console.log(this.rounds);
 
         this.play();
     }
-    updateCounter() {
-        this.counter.innerHTML = `${this.currentRound}/${this.rounds.length}`;
-    }
+    // updateCounter() {
+    //     while (true) {
+    //         if (this.sortingArray[this.sortingCurrentBin] === undefined) {
+    //             console.log("Complete");
+    //             // TODO
+    //             return;
+    //         }
+    //         if (this.sortingArray[this.sortingCurrentBin]?.isComplete()) {
+    //             this.sortingCurrentBin++;
+    //         } else {
+    //             // We have found a bin that still needs sorting
+    //             break;
+    //         }
+    //     }
+
+    //     // TODO: Update max rounds
+    //     let remainingIntervals = 0;
+    //     for (let i = this.sortingCurrentBin; i < (this.sortingArray.length as number); i++) {
+    //         remainingIntervals += this.sortingArray[this.sortingCurrentBin]?.values.length as number;
+    //     }
+    //     this.maxRounds = (remainingIntervals * (remainingIntervals - 1)) / 2;
+
+    //     this.counter.innerHTML = `${this.currentRound}/${this.maxRounds}`;
+    // }
     play() {
-        if (this.rounds[this.currentRound] === undefined) {
-            console.warn(`Trying to access a non-existant round ${this.currentRound}`);
+        while (true) {
+            if (this.sortingArray[this.sortingCurrentBin] === undefined) {
+                console.log("Complete");
+                this.sortingComplete = true;
+                this.finish();
+                // TODO
+                return;
+            }
+            if (this.sortingArray[this.sortingCurrentBin]?.isComplete()) {
+                this.sortingCurrentBin++;
+            } else {
+                // We have found a bin that still needs sorting
+                break;
+            }
         }
-        const currentRound = this.rounds[this.currentRound] as number[];
+
+        // TODO: Update max rounds
+        let remainingIntervals = 0;
+        for (let i = this.sortingCurrentBin; i < (this.sortingArray.length as number); i++) {
+            remainingIntervals += this.sortingArray[this.sortingCurrentBin]?.values.length as number;
+        }
+        this.maxRounds = (remainingIntervals * (remainingIntervals - 1)) / 2;
+
+        // Update counter that is visible to the user
+        this.counter.innerHTML = `${this.currentRound}/${this.maxRounds}`;
+
+        // const currentRound = this.rounds[this.currentRound] as number[];
         const noteDuration_ms = 750;
         // Play the first sound
-        this.playInterval(currentRound[0] as number, 0, noteDuration_ms);
+        this.playInterval(this.sortingArray[this.sortingCurrentBin]?.getNextValue() as number, 0, noteDuration_ms);
         (this.buttons[0] as HTMLElement).classList.add("playing");
         setTimeout(() => {
             (this.buttons[0] as HTMLElement).classList.remove("playing");
         }, noteDuration_ms);
 
         // Play the second sound
-        this.playInterval(currentRound[1] as number, noteDuration_ms + 250, noteDuration_ms);
+        this.playInterval(this.sortingArray[this.sortingCurrentBin]?.getPivot() as number, noteDuration_ms + 250, noteDuration_ms);
         setTimeout(() => {
             (this.buttons[1] as HTMLElement).classList.add("playing");
         }, noteDuration_ms + 250);
@@ -154,16 +243,39 @@ class Game {
         });
     }
     select(key: string) {
+        if (this.sortingComplete) {
+            this.finish();
+            return;
+        }
+        // Check if current bin is sorted
+        // const pivot = this.sortingArray[this.sortingCurrentBin]?.getPivot() as number;
+        // Perform comparison
+        let v = this.sortingArray[this.sortingCurrentBin]?.popNextValue() as number;
+
         let element;
         switch (key) {
             case "1":
                 element = this.buttons[0];
+                if (this.sortingLessBin === false) {
+                    this.sortingArray.splice(this.sortingCurrentBin, 0, new Bin([v]));
+                    this.sortingLessBin = true;
+                    this.sortingCurrentBin++;
+                } else {
+                    this.sortingArray[this.sortingCurrentBin - 1]?.push(v);
+                }
                 break;
             case "2":
                 element = this.buttons[1];
+                if (this.sortingMoreBin === false) {
+                    this.sortingArray.splice(this.sortingCurrentBin + 1, 0, new Bin([v]));
+                    this.sortingMoreBin = true;
+                } else {
+                    this.sortingArray[this.sortingCurrentBin + 1]?.push(v);
+                }
                 break;
             case "3":
                 element = this.buttons[2];
+                this.sortingArray[this.sortingCurrentBin]?.addSame(v);
                 break;
             case " ":
                 this.play();
@@ -174,19 +286,27 @@ class Game {
         if (element === undefined) {
             return;
         }
+
         element.classList.add("selected");
         setTimeout(() => {
             element.classList.remove("selected");
         }, 100);
-        if (this.currentRound++ >= this.rounds.length) {
-            this.finish();
-        } else {
-            this.updateCounter();
-            this.play();
+
+        // Check if bin is complete
+        if (this.sortingArray[this.sortingCurrentBin]?.isComplete()) {
+            // If it is, then reset the counter so that we can find the bin with the lowest values
+            this.sortingCurrentBin = 0;
+            this.sortingLessBin = false;
+            this.sortingMoreBin = false;
         }
+
+        this.currentRound++;
+
+        this.play();
     }
     finish() {
         console.log("Game complete");
+        console.log(this.sortingArray);
     }
 }
 
@@ -196,11 +316,12 @@ export function initGame() {
     let game = new Game(audioContext, 10);
 
     // Hide the start button and show all of the elements
-    (document.getElementById("start-game") as HTMLElement).hidden = true;
     let gameElements = document.querySelectorAll(".game-elements");
     for (let i = 0; i < gameElements.length; i++) {
         (gameElements[i] as Element).classList.remove("game-elements");
     }
+
+    (document.getElementById("start-screen") as HTMLElement).classList.add("game-elements");
 
     document.addEventListener("keydown", (e) => {
         game.select(e.key);
